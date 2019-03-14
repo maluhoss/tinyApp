305892aca65a90340ea85ab636930ceaa86983df
Maliha Hossain <malu.hoss@gmail.com>
2019-03-14 19:25:03 +0000

    added get and post route endpoint for registering, also changed username to email in js and partial header

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
+//generate shortURL
 function generateRandomString() {
   let alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   let numericString = '';
@@ -21,48 +22,80 @@ function generateRandomString() {
 key-value pair are saved to the urlDatabase when it receives a
 POST request to /urls*/
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//homepage of all urls
 app.get("/urls", (req, res) => {
   let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    email: req.cookies["email"]
   };
   res.render("urls_index", templateVars);
 });
 
+//creating new url to add to homepage of urls
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect("/urls/" + shortURL);
 });
 
-// app.get("/login", (req, res) => {
-//   res.redirect("/urls");
-// })
-
+//creating cookie when logging in
 app.post("/login", (req, res) => {
-  let username = req.body.username;
-  res.cookie("username", username);
+  let email = req.body.email;
+  res.cookie("email", email);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("email");
   res.redirect("/urls");
 })
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"]}
+  let templateVars = { email: req.cookies["email"]}
   res.render("urls_new", templateVars);
+});
+
+
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
+
+//adding user to user object and creating cookie
+app.post("/register", (req, res) => {
+  const user_id = generateRandomString();
+  users[user_id] = {
+    id: user_id,
+    email: req.body.email,
+    password: req.body.password
+  };
+  console.log(req.body)
+  res.cookie("user_id", user_id);
+  console.log(req.cookies);
+  res.redirect("/urls");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -70,7 +103,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
-    username: req.cookies["username"]
+    email: req.cookies["email"]
   };
   res.render("urls_show", templateVars);
 });
